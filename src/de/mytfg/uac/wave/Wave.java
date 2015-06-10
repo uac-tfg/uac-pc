47e00833fc89ada35f620b58c89b8dc04783029e
Tilman Hoffbauer <tilman@lewe-hoffbauer.de>
2015-06-10 17:25:13 +0200

    Further work on Wave
    - made it one-channel
    - implemented addWave, buggy

--- src/de/mytfg/uac/wave/Wave.java
+++ src/de/mytfg/uac/wave/Wave.java
@@ -10,13 +10,15 @@ import de.mytfg.uac.wave.wav.WavFileException;
 public class Wave {
 
   private static final int BUFFER_SIZE = 1024;
-  
+
   private RandomAccessWavFile wav;
 
   public Wave(File file, WaveConfig config) {
     try {
-      this.wav = RandomAccessWavFile.newWavFile(file, config.getNumChannels(), 
-          config.getNumFrames(), config.getValidBits(), config.getSampleRate());
+      this.wav =
+          RandomAccessWavFile.newWavFile(file, config.getNumChannels(), config.getNumFrames(),
+              config.getValidBits(), config.getSampleRate());
+      clear(); // allocate file
     } catch (IOException | WavFileException e) {
       throw new RuntimeException(e);
     }
@@ -30,12 +32,12 @@ public class Wave {
     }
   }
 
-  public double[][] getFrames(long from, int length) {
+  public double[] getFrames(long from, int length) {
     if (from < 0 || from + length > getNumFrames()) {
       throw new IndexOutOfBoundsException();
     }
 
-    double[][] samples = new double[getNumChannels()][length];
+    double[][] samples = new double[1][length];
     int read;
     try {
       wav.seek(from, 0);
@@ -45,62 +47,53 @@ public class Wave {
     }
     if (read != length)
       throw new RuntimeException("Reached end of file before all samples were read!");
-    return samples;
+    return samples[0];
   }
 
-  public double[] getSamples(int channel, long from, int length) {
-    if (channel < 0 || channel > this.getNumChannels()) {
-      throw new IndexOutOfBoundsException(String.valueOf(channel));
-    }
-    return getFrames(from, length)[channel];
-  }
-
-  public void setFrames(long from, double[][] samples) {
+  public void setFrames(long from, double[] samples) {
     Objects.requireNonNull(samples);
-    if (samples.length != getNumChannels())
-      throw new IllegalArgumentException("Given sample does not have the same amount of channels!");
-    int numFrames = samples[0].length;
-    if (from < 0 || from + numFrames > getNumFrames())
+    int numFrames = samples.length;
+    if (from < 0 || from + numFrames > getNumFrames()) {
+      System.out.println(numFrames + " | " + from);
       throw new IndexOutOfBoundsException(String.valueOf(from));
+    }
+
+    double[][] buffer = new double[][] {samples};
 
     try {
       wav.seek(from, 0);
-      wav.writeFrames(samples, numFrames);
+      wav.writeFrames(buffer, numFrames);
     } catch (IOException | WavFileException e) {
       throw new RuntimeException("Error while writing to underlying .wav!", e);
     }
   }
-  
-  public void setSamples(int channel, long from, double[] samples) {
-    double[][] d = getFrames(from, samples.length);
-    d[channel] = samples;
-    setFrames(from, d);
-  }
-  
-  public void setSamples(long from, double[] samples) {
-    setSamples(0, from, samples);
-  }
 
-  public void addWave(int fromChannel, int toChannel, Wave wave, long fromStart, long toStart, 
-      long length) {
-    if (toChannel < 0 || toChannel > this.getNumChannels()) {
-      throw new IndexOutOfBoundsException(String.valueOf(toChannel));
-    }
-    if (fromChannel < 0 || fromChannel > wave.getNumChannels()) {
-      throw new IndexOutOfBoundsException(String.valueOf(fromChannel));
-    }
+  public void addWave(Wave wave, long toStart, long fromStart, long length) {
     double[] fromBuffer = null;
     double[] toBuffer = null;
     int pointer = BUFFER_SIZE;
     for (long i = 0; i < length; i++, pointer++) {
-      if (pointer == BUFFER_SIZE) {
-        setSamples(toChannel, i + toStart, toBuffer);
-        
+      if (pointer >= BUFFER_SIZE) {
+        if (fromBuffer != null && toBuffer != null) {
+          setFrames(i + toStart - toBuffer.length, toBuffer);
+        }
+
         int size = (int) Math.min(BUFFER_SIZE, length - i);
-        fromBuffer = wave.getSamples(fromChannel, i + fromStart, size);
-        toBuffer = this.getSamples(toChannel, i + toStart, size);
+        fromBuffer = wave.getFrames(i + fromStart, size);
+        toBuffer = this.getFrames(i + toStart, size);
+        pointer = 0;
       }
       toBuffer[pointer] = toBuffer[pointer] + fromBuffer[pointer];
+    }
+  }
+
+  public void clear() {
+    int toWrite;
+    long i;
+    for (i = 0; i < getNumFrames(); i += toWrite) {
+      toWrite = (int) Math.min(BUFFER_SIZE, getNumFrames() - i);
+      double[] buffer = new double[toWrite];
+      setFrames(i, buffer);
     }
   }
 
@@ -124,10 +117,6 @@ public class Wave {
     return (1.0 / wav.getSampleRate()) * wav.getNumFrames();
   }
 
-  public int getNumChannels() {
-    return wav.getNumChannels();
-  }
-
   public int getValidBits() {
     return wav.getValidBits();
   }
@@ -139,7 +128,7 @@ public class Wave {
   public long getCurrentFrame() {
     return wav.getNumFrames() - wav.getFramesRemaining();
   }
-  
+
   public File getFile() {
     return wav.getFile();
   }
