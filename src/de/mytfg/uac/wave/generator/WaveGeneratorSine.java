846db0ab84119d9c876d256e54cca5ca40b90167
tilman <tilman@lewe-hoffbauer.de>
2015-07-15 16:56:37 +0200

    Add phase to sine generation

--- src/de/mytfg/uac/wave/generator/WaveGeneratorSine.java
+++ src/de/mytfg/uac/wave/generator/WaveGeneratorSine.java
@@ -5,14 +5,37 @@ import de.mytfg.uac.wave.Wave;
 public class WaveGeneratorSine extends WaveGenerator {
 
   private final int frequency;
-  
+  private final double phase;
+
+  /**
+   * Constructs a new WaveGenerator for sines. The phase is defaulted to 0 (begin of a period).
+   * 
+   * @param frequency the frequency of the wanted sin
+   */
   public WaveGeneratorSine(int frequency) {
-    this.frequency = frequency;
+    this(frequency, 0);
   }
-  
+
+  /**
+   * Constructs a new WaveGenerator for sines.
+   * 
+   * @param frequency the frequency of the wanted sine
+   * @param phase the phase decoded in a value from 0 to 1 where 0 is the begin of a period and 1 is
+   *        the end of a period
+   */
+  public WaveGeneratorSine(int frequency, double phase) {
+    this.frequency = frequency;
+    this.phase = phase;
+  }
+
   @Override
   protected double generateSample(Wave wave, long abs, long rel, long left) {
-    return Math.sin(frequency * 2 * Math.PI * abs * (1d / wave.getSampleRate()));
+    double period = frequency * 2 * Math.PI;
+    double time = abs * (1d / wave.getSampleRate());
+    double shift = 2 * Math.PI * phase;
+    double val = Math.sin(period * time + shift);
+    return val;
+//    return Math.sin(frequency * 2 * Math.PI * abs * (1d / wave.getSampleRate()));
   }
 
 }
